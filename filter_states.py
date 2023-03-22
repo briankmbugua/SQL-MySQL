5cff0d5270871e73adc427af071f315346466289
briankmbugua <briankmbuguak@gmail.com>
2023-03-22 23:22:43 +0300

    mysqldb

---
+++ filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+#should not be executed when imported
+if __name__ == '__main__':
+    #create connection to the database
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],db=sys.argv[3])
+    userInput = sys.argv[4]
+    #create a cursor object
+    c = db.cursor()
+    sql = f"SELECT id, name FROM states WHERE name='{userInput}'"
+    c.execute(sql)
+    for result in c.fetchall():
+        print(result)
+    
--- select_states.py
+++ select_states.py
@@ -1,27 +1,14 @@
 #!/usr/bin/python3
 # """List all states from the database hbtn_0e_0_usa"""
-# import MySQLdb
-# import sys
-# #connect to the database(MySQL)
-# db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],db=sys.argv[3])
-# #create the cursor object
-# c = db.cursor()
-# #Executing an SQL statement
-# c.execute("SELECT * FROM `states`")#The result will be on the cursor object
-# #Usig the fetchall() method of the cursor object to fetch all the results of the query
-# for state in c.fetchall():
-#     print(state)
-
-
-"""
-Module that has a scripts that lists all states from
-the database hbtn_0e_0_usa
-"""
-import sys
 import MySQLdb
+import sys
+#connect to the database(MySQL)
+db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],db=sys.argv[3])
+#create the cursor object
+c = db.cursor()
+#Executing an SQL statement
+c.execute("SELECT * FROM `states`")#The result will be on the cursor object
+#Usig the fetchall() method of the cursor object to fetch all the results of the query
+for state in c.fetchall():
+    print(state)
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
